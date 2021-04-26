eb132cef4c95afbda74bd21fb8fcfc8bfceb188c
lucio <lucioalema@gmail.com>
2021-04-26 20:24:33 -0300

    UserMicroservices added

---
+++ Bank.Auth.Infrastructure/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Auth.Domain.Users;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Bank.Auth.Infrastructure.Services
+{
+    public class UserService :IUserService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly string symmetricKey;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            this.symmetricKey = "123456ABC123456ABC";
+        }
+
+        public async Task<string> Authenticate(string userName, string password)
+        {
+            var user = await userRepository.FindByUserName(userName);
+            if (user == null)
+                return null;
+
+            if (!user.PasswordMatches(password))
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(symmetricKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("sub", user.UserName),
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    //new Claim(ClaimTypes.Role, "SALESMAN"),
+                    //new Claim(ClaimTypes.Role, "USER"),
+                    new Claim("avatar", user.Avatar),
+                    //new Claim("userType", "SALESMAN"),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Bank.Auth/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+
+namespace Bank.Auth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration)
+                                .EnableTokenAcquisitionToCallDownstreamApi()
+                                    .AddMicrosoftGraph(Configuration.GetSection("DownstreamApi"))
+                                    .AddInMemoryTokenCaches();
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(opt =>
+            //    {
+            //        opt.Audience = Configuration["AAD:ResourceId"];
+            //        opt.Authority = $"{Configuration["AAD:Instance"]}{Configuration["AAD:TenantId"]}";
+            //    });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
