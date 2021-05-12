b1837c06043ce306aa61aef640a303ed4de93000
lucio <lucioalema@gmail.com>
2021-05-12 00:56:51 -0300

    -

---
+++ kubernetes/app.install.sh
@@ -0,0 +1,7 @@
+﻿kubectl apply -f bank.auth.deployment.yml
+kubectl apply -f bank.loans.deployment.yml
+kubectl apply -f bank.apigateway.deployment.yml
+
+kubectl apply -f bank.auth.service.yml
+kubectl apply -f bank.loans.service.yml
+kubectl apply -f ingress.service.yml
---
+++ kubernetes/app.uninstall.sh
@@ -0,0 +1,7 @@
+﻿kubectl delete -f bank.auth.deployment.yml
+kubectl delete -f bank.loans.deployment.yml
+kubectl delete -f bank.apigateway.deployment.yml
+
+kubectl delete -f bank.auth.service.yml
+kubectl delete -f bank.loans.service.yml
+kubectl delete -f ingress.service.yml
